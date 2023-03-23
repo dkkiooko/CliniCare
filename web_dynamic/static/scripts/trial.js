789635f91fc687ad23e8766031dfc5a73c564cd9
bek2518 <bereketb20@hotmail.com>
2023-03-23 11:08:24 +0300

    Edited jquery on development

--- web_dynamic/static/scripts/trial.js
+++ web_dynamic/static/scripts/trial.js
@@ -1,202 +1,21 @@
-const $ = window.$;
-const store = {};
 $(document).ready(function() {
-  $('section.visit_input').change(function() {
-	if (this.checked) {
-		store[$(this).attr('data-id')] = $(this).attr('data-name');
-	  } else {
-		delete store[$(this).attr('data-id')];
-	  }
-	  if (Object.keys(store).length > 0) {
-		$('.date-filter h4').text(Object.values(store).join(', '));
-	  }
-	});
 
-  $('button').click(function() {
-    $.ajax({
-      type: 'GET',
-      url: 'http://0.0.0.0:5001/api/v1/visits',
-      contentType: 'application/json',
-	  dataType: 'json',
-	  success: function (data, textStatus) {
-		if (textStatus === 'success') {
-			$('table.patient-tab').remove();
-			data.forEach((visit) => $('section.patient-info').append(
-				`
-				<table class="patient-tab">
-				<form>
-					<tr>
-						<td>
-							<label for="preexisting">Pre-existing Conditions</label>
-						</td>
-						<td colspan="3">
-							<output name="preexisting">${visit[0].pre_existing_conditions}</output>
-							<!-- (value="value needed" readonly) code to be 
-							added to make input section sidplay value and be readonly-->
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<label for="history">History</label>
-						</td>
-						<td colspan="3">
-							<output name="history">${visit.history}</output>
-						</td>
-					</tr>
-					<tr>
-						<td colspan="2">
-							<h2>Vital Signs and Anthropometry</h2>
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<label for="hr">Heart Rate:</label>
-						</td>
-						<td>
-							<output name="hr">${visit.heart_rate}</output>
-						</td>
-						<td>
-							<label for="bp">Blood Pressure:</label>
-						</td>
-						<td>
-							<output name="bp">${visit.blood_pressure}</output>
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<label for="rr">Respiratory Rate:</label>
-						</td>
-						<td>
-							<output name="rr">${visit.respiratory_rate}</output>
-						</td>
-						<td>
-							<label for="temp">Temprature:</label>
-						</td>
-						<td>
-							<output name="temp">${visit.temprature}</output>
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<label for="height">Height:</label>
-						</td>
-						<td>
-							<output name="height">${visit.height}</output>
-						</td>
-						<td>
-							<label for="temp">Weight:</label>
-						</td>
-						<td>
-							<output name="weight">${visit.weight}</output>
-						</td>
-					</tr>
-					<tr>
-						<td colspan="2">
-							<h2>Physical Examination</h2>
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<label for="heent">HEENT</label>
-						</td>
-						<td>
-							<output name="heent">${visit.heent}</output>
-						</td>
-						<td>
-							<label for="abd">Abdomen</label>
-						</td>
-						<td>
-							<output name="abd">${visit.abdomen}</output>
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<label for="lg">Lymphoglandular</label>
-						</td>
-						<td>
-							<output name="lg">${visit.lymphoglandular}</output>
-						</td>
-						<td>
-							<label for="gu">Genitourinary</label>
-						</td>
-						<td>
-							<output name="gu">${visit.genitourinary}</output>
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<label for="chest">Chest</label>
-						</td>
-						<td>
-							<output name="chest">${visit.chest}</output>
-						</td>
-						<td>
-							<label for="integ">Integumentary</label>
-						</td>
-						<td>
-							<output name="integ">${visit.integumentary}</output>
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<label for="cvs">Cardiovascular</label>
-						</td>
-						<td>
-							<output name="cvs">${visit.cardiovascular}</output>
-						</td>
-						<td>
-							<label for="mss">Musculoskeletal</label>
-						</td>
-						<td>
-							<output name="mss">${visit.musculoskeletal}</output>
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<label for="add">Additional</label>
-						</td>
-						<td colspan="2">
-							<output name="add">${visit.others}</output>
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<label for="lab">Laboratory Investigations</label>
-						</td>
-						<td colspan="3">
-							<output name="lab">${visit.lab_investigations}</output>
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<label for="dx">Diagnosis</label>
-						</td>
-						<td colspan="3">
-							<output name="dx">${visit.diagnosis}</output>
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<label for="med">Medications</label>
-						</td>
-						<td colspan="3">
-							<output name="med">${visit.current_medication}</output>
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<label for="physician">Attending Physician:</label>
-						</td>
-						<td>
-							<output name="physician">${visit.doctor_name}</output>
-						</td>
-					</tr>
-				</form>
-				</table>
-				`
-			));
-		}
-	  }
+  store = {}
+  $('section.date-filter ul li input').change(function() {
+    if (this.checked) {
+      store[$(this).attr('data-id')] = $(this).attr('data-name');
+    }
+    else {
+      delete store[$(this).attr('data-id')];
+    }
+    $('.date-filter h4').text(Object.values(store).join(', '));
+  });
+
+  $('#search-date').click(function() {
+    $.get('http://127.0.0.1:5001/api/v1/status/', function (data) {
+      if (data.status === 'OK') {
+        $('section.date-filter h4').text('jQuery is working!');
+      }
     });
   });
 });
