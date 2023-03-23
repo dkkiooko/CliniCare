ee7c3ed550918bf787dde17b9e214270640a39d8
dkkiooko <kithekadankioko@gmail.com>
2023-03-23 12:56:40 +0300

    removed unneccessary files

--- reset.py
+++
@@ -1,9 +0,0 @@
-import csv
-import pandas as pd
-with open('visits.csv', 'r') as infile, open('visits3.csv', 'a') as outfile:
-	fieldnames = ['', 'created_at', 'updated_at', 'id', 'patient_id', 'doctor_id', 'doctor_name', 'name', 'email_address', 'patient_age', 'patient_gender', 'phone_number', 'price_charged', 'city', 'sub_city', 'county', 'location', 'pre_existing_conditions', 'history', 'heart_rate', 'blood_pressure', 'height', 'weight', 'respiratory_rate', 'temperature', 'heent', 'genitourinary', 'lymphoglandular', 'integumentary', 'chest', 'muscoskeletal', 'cardiovascular', 'abdomen', 'others', 'lab_investigations', 'current_medication', 'diagnosis']
-	writer = csv.DictWriter(outfile, fieldnames=fieldnames)
-	writer.writeheader()
-	for row in csv.DictReader(infile):
-		writer.writerow(row)
-
--- setup_mysql_test.sql
+++
@@ -1,7 +0,0 @@
--- prepares a MySQL server for the project
-
-CREATE DATABASE IF NOT EXISTS clinicare_test_db;
-CREATE USER IF NOT EXISTS 'care_test'@'localhost' IDENTIFIED BY 'clinic_test_pwd';
-GRANT ALL PRIVILEGES ON `clinic_test_db`.* TO 'care_test'@'localhost';
-GRANT SELECT ON `performance_schema`.* TO 'care_test'@'localhost';
-FLUSH PRIVILEGES;
