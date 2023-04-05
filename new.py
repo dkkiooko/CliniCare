3a73feb9a7f333e9b2e220483f0d1832c6fd8334
dkkiooko <kithekadankioko@gmail.com>
2023-04-05 22:25:19 +0300

    deleted the unneccessary pycache files

--- new.py
+++
@@ -1,7 +0,0 @@
-from models import storage
-from models.visit import Visit
-from models.doctor import Doctor
-from models.patient import Patient
-
-visits = storage.get_patient_provider(Patient, patient_id)
-print(visits)
